721bba8b9b1e8915cb359f169564972f7082e0d0
fibo <casati_gianluca@yahoo.it>
2016-08-31 10:48:02 +0200

    :cyclone: using git clone --recursive

--- fun.sh
+++ fun.sh
@@ -1,56 +1,56 @@
 bb-clone() {
-	if [ -z "$1" ]
-	then
-		cat <<-EOF
-		# Bitbucket clone repo util
-		##
-		# Installation instructions, source and license available here:
-		# https://github.com/fibo/bb-clone#bb-clone
-		##
-		USAGE: bb-clone [user/]repo
-		EOF
-		return 0
-	fi
-	BITBUCKET_DIR=${BITBUCKET_DIR:-~/bitbucket.org}
-	MY_BITBUCKET_USER=$(git config --global bitbucket.user)
-	if [ -z "$MY_BITBUCKET_USER" ]
-	then
-		read -p "Enter your bitbucket.user: " MY_BITBUCKET_USER
-		git config --global bitbucket.user $MY_BITBUCKET_USER
-	fi
-	BITBUCKET_USER=$(echo $1 | cut -d / -f1)
-	REPO_NAME=$(echo $1 | cut -d / -f2)
-	if [ "$REPO_NAME" == "$BITBUCKET_USER" ]
-	then
-		BITBUCKET_USER=$MY_BITBUCKET_USER
-	fi
-	TARGET_DIR=$BITBUCKET_DIR/$BITBUCKET_USER
-	mkdir -p $TARGET_DIR
-	cd $TARGET_DIR
-	REPO_URL=git@bitbucket.org:$BITBUCKET_USER/${REPO_NAME}.git
-	git clone $REPO_URL && cd $REPO_NAME
-	if [ -e package.json ]
-	then
-		NPM=$(which npm 2> /dev/null)
-		if [ ! -z "$NPM" ]
-		then
-			$NPM install
-		fi
-	fi
-	if [ -e bower.json ]
-	then
-		BOWER=$(which bower 2> /dev/null)
-		if [ ! -z "$BOWER" ]
-		then
-			$BOWER install
-		fi
-	fi
-	unset BOWER
-	unset BITBUCKET_DIR
-	unset BITBUCKET_USER
-	unset MY_BITBUCKET_USER
-	unset NPM
-	unset REPO_NAME
-	unset REPO_URL
-	unset TARGET_DIR
+tif [ -z "$1" ]
+tthen
+t	cat <<-EOF
+t	# Bitbucket clone repo util
+t	##
+t	# Installation instructions, source and license available here:
+t	# https://github.com/fibo/bb-clone#bb-clone
+t	##
+t	USAGE: bb-clone [user/]repo
+t	EOF
+t	return 0
+tfi
+tBITBUCKET_DIR=${BITBUCKET_DIR:-~/bitbucket.org}
+tMY_BITBUCKET_USER=$(git config --global bitbucket.user)
+tif [ -z "$MY_BITBUCKET_USER" ]
+tthen
+t	read -p "Enter your bitbucket.user: " MY_BITBUCKET_USER
+t	git config --global bitbucket.user $MY_BITBUCKET_USER
+tfi
+tBITBUCKET_USER=$(echo $1 | cut -d / -f1)
+tREPO_NAME=$(echo $1 | cut -d / -f2)
+tif [ "$REPO_NAME" == "$BITBUCKET_USER" ]
+tthen
+t	BITBUCKET_USER=$MY_BITBUCKET_USER
+tfi
+tTARGET_DIR=$BITBUCKET_DIR/$BITBUCKET_USER
+tmkdir -p $TARGET_DIR
+tcd $TARGET_DIR
+tREPO_URL=git@bitbucket.org:$BITBUCKET_USER/${REPO_NAME}.git
+tgit clone --recursive $REPO_URL && cd $REPO_NAME
+tif [ -e package.json ]
+tthen
+t	NPM=$(which npm 2> /dev/null)
+t	if [ ! -z "$NPM" ]
+t	then
+t		$NPM install
+t	fi
+tfi
+tif [ -e bower.json ]
+tthen
+t	BOWER=$(which bower 2> /dev/null)
+t	if [ ! -z "$BOWER" ]
+t	then
+t		$BOWER install
+t	fi
+tfi
+tunset BOWER
+tunset BITBUCKET_DIR
+tunset BITBUCKET_USER
+tunset MY_BITBUCKET_USER
+tunset NPM
+tunset REPO_NAME
+tunset REPO_URL
+tunset TARGET_DIR
 }
